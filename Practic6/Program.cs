0e1369a5683b0f1fe305d286bdf61551299ad368
Rvmzesbro <galeevramazan64@gmail.com>
2025-03-18 12:04:56 +0300

    lastcommit

--- Practic6/Program.cs
+++ Practic6/Program.cs
@@ -10,18 +10,19 @@ namespace Practic6
     {
         static void Main(string[] args)
         {
+            // Сотрудник
             Employee employee = new Employee("Иванов Иван Иванович", 52, 52000);
             employee.CalculateBonus();
             employee.GetInformation();
-
+            // Менеджер
             Manager manager = new Manager("Петров Петр Петрович", 36, 45000, 5);
             manager.BonusManager();
             manager.ShowInfo();
-
+            // Программист
             Developer developer = new Developer("Сергеев Сергей Сергеевич", 25, 180000, "С#");
             developer.BonusProgrammer();
             developer.ShowInfo();
-
+            // Стажер
             Intern intern = new Intern("Алексеев Алексей Алексеевич", 17, 30000, "Ebay");
             intern.BonusUniversity();
             intern.ShowInfo();
